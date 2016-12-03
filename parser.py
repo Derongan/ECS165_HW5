20f4cdd56d56d4c0053890ece6bd401d5fb52a48
Rafferty Hutari Askara <rafferty.askara@gmail.com>
2016-12-02 17:56:49 -0800

    Update parser.py
    Skips if no student in course,
    Set up for meeting table and course table.
    # don't forget to check the comments in program.

--- parser.py
+++ parser.py
@@ -3,10 +3,15 @@ import os
 
 
 def parse_quarter(filename):
-    courses = []
+    all_courses = []
+
+    last_prof = None
 
     last_course = {}
+    temp_meet = {}
 
+    courses = {}
+    meeting = {}
     student = {}
     student_quarter_data = {}
 
@@ -29,6 +34,24 @@ def parse_quarter(filename):
                 last_course['data'] = row
                 reading_course = False
             elif reading_instructor:
+                data = last_course["data"]
+                # can change building and room to instructor
+                # no instructor can be at two places at the same day and time
+                # but check for overlapping days
+
+                if first:
+                    last_prof = first
+                temp_meet[(data[1], row[2], row[3], row[4], row[5])] = {
+                    'term': data[1],
+                    'time': row[2],
+                    'days': row[3],
+                    'building': row[4],
+                    'room': row[5],
+                    'instructor': last_prof,
+                    'type': row[1],
+                    'cid': data[0],
+                    'section': data[4]
+                }
                 last_course['meetings'].append(row)
             elif reading_student:
                 # Commented out test that made sure emails always match on IDs. They do. Yay.
@@ -46,17 +69,35 @@ def parse_quarter(filename):
                 last_course['students'].append(row)
             elif first == 'CID':
                 reading_course = True
-                if last_course:
-                    if last_course['students']: #ignores courses which has no students in it
-                        courses.append(last_course)
+                if last_course and not last_course['students']:
+                    data = last_course["data"]
+                    courses[(data[0], data[1], data[4])] = {
+                        'cid': data[0],
+                        'term': data[1],
+                        'section': data[4],
+                        'subject': data[2],
+                        'crse': data[3],
+                        'units': data[-1]  # needs formatting as range
+                    }
+                    meeting.update(temp_meet)
+
+                    all_courses.append(last_course)
+
                 last_course = {'students': [], 'meetings': []}
+                temp_meet = {}
+                last_prof = None
             elif first == 'INSTRUCTOR(S)':
                 reading_instructor = True
             elif first == 'SEAT':
                 reading_student = True
 
-        courses.append(last_course)
-        return {'student': student}
+        all_courses.append(last_course)
+        return {
+            'student': student,
+            'studentquarterdata': student_quarter_data,
+            'course': courses,
+            'meeting': meeting
+        }
 
 
 if __name__ == "__main__":
